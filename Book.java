8c2265915708387bfe94318b7c2d19fcb684d410
toracgyuzerocgyu-cmd <toracgyu.zerocgyu@gmail.com>
2026-08-20 11:50:53 +0900

    test

--- Book.java
+++ Book.java
@@ -1,7 +1,7 @@
 public class Book extends TangibleAsset{
 	private String isbn;
 	public Book(String name, int price, String color, String isbn){
-		super();
+		super(name, color, isbn);
 		this.isbn = isbn;
 	}
 	public String getIsbn(){return this.isbn;}
