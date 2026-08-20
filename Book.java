80d54d0df51c9b63eae13de737a2870fdedab5f0
toracgyuzerocgyu-cmd <toracgyu.zerocgyu@gmail.com>
2026-08-20 11:13:38 +0900

    superの引数削除

--- Book.java
+++ Book.java
@@ -1,7 +1,7 @@
 public class Book extends TangibleAsset{
 	private String isbn;
 	public Book(String name, int price, String color, String isbn){
-		super(name, price, color);
+		super();
 		this.isbn = isbn;
 	}
 	public String getIsbn(){return this.isbn;}
